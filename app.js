16c73e9d2ecb6ce30c3f755cddf2113747c1fa6c
Moopey <tijn122@gmail.com>
2025-10-09 21:08:20 +0200

    Dynamic route works

--- app.js
+++ app.js
@@ -11,11 +11,19 @@ app.engine('html', require('ejs').__express);
 app.set('view engine', 'html');
 
 app.get('/', (req, res) => {
-
   res.render('home', {
     cardData: cardData
   });
 
+  console.log('Rendering home page with card data:', cardData);
+});
+
+app.get('/:blog', (req, res) => {
+  console.log(req.params);
+  const blogWithoutQuotes = JSON.stringify(req.params.blog).replace(/"/g, '');
+  res.render('blogPage', {
+    blog: blogWithoutQuotes
+  });
 });
 
 app.listen(port, () => {
