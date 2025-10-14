99184a99cb061c1411a578e1d251a33aac169432
tijn122@gmail.com <tijn122@gmail.com>
2025-10-14 13:16:07 +0200

    card sorter works!

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, '/public')));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
 app.engine('html', require('ejs').__express);
@@ -19,18 +21,32 @@ app.get('/', (req, res) => {
   res.render('home', {
     cardData: cardData
   });
-
-  console.log('Rendering home page with card data:', cardData);
 });
 
-app.get('/:blog', (req, res) => {
-  const blog = cardData.find(b => b.url === req.params.blog);
+const cardSorter = (req, res, next) => {
+  const sortedArray = [];
+  const parsedTag = req.params.param.split('=')[1];
+  cardData.forEach((object) => {
+    object.tags.forEach((tag) => {
+      if (tag === parsedTag) {
+        sortedArray.push(object);
+      }
+    });
+  });
+
+  res.render('home', {
+    cardData: sortedArray
+  });
+  next();
+};
+
+const mdLoader = (req, res, next) => {
+  const blog = cardData.find(b => b.url === req.params.param);
   if (!blog) {
     res.status(404).send("Sorry can't find that!");
   }
-  
-  const file = matter.read(__dirname + '/blogs/' + req.params.blog + '.md');
-  console.log('dit is file' + file);
+
+  const file = matter.read(__dirname + '/blogs/' + req.params.param + '.md');
 
   const md = require('markdown-it')();
   let content = file.content;
@@ -41,6 +57,19 @@ app.get('/:blog', (req, res) => {
     title: file.data.title,
     description: file.data.description
   });
+  next();
+};
+
+const checker = (req, res, next) => {
+  if (req.params.param.includes('tag')) {
+    cardSorter(req, res, next);
+  } else {
+    mdLoader(req, res, next);
+  }
+};
+
+app.get('/:param', checker, (req, res) => {
+  console.log(req.params.param);
 });
 
 app.listen(port, () => {
